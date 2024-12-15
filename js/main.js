f5b5f90786563b917a134d3f174be9392b1c14c0
Mohamedsamid <ccbkivvg@gmail.com>
2024-12-15 22:28:56 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,8 @@
+let img = document.querySelector('.img');
+let container = document.querySelector('.container');
+function phones(phpne){
+    img.src = phone;
+}
+function colors(color){
+    container.Style.backgroud = color;
+}
